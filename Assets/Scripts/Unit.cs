7cb850b49fdafa471cc56ddc206354c6fd38686c
Zayniddin <89527543+FlashThompson1@users.noreply.github.com>
2025-03-03 15:52:10 +0500

    Added new units and animations, updated unit panel.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    public enum UnitClass { Swordsman, Archer } // Классы юнитов
+    public enum UnitClass { Swordsman, Archer , Shaman, Brute } // Классы юнитов
     public UnitClass unitClass;
     public Sprite unitIcon; // Иконка юнита
 
