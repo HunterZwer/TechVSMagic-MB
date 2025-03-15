148dcb293d91df1452ec27b0920faf55f87f0fa7
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2025-03-16 03:03:02 +0500

    TestUi panel for other scenes, Pause on button

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 public class PauseScript : MonoBehaviour
 {
     [SerializeField] private GameObject pausePanel;
+    private bool isPaused = false;
 
-    
+    void Update()
+    {
+        // Check for Escape key press
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Toggle pause state
+            if (isPaused)
+                ContinueButton();
+            else
+                PauseButton();
+        }
+    }
 
     public void PauseButton()
     {
@@ -12,13 +24,17 @@ public class PauseScript : MonoBehaviour
         pausePanel.SetActive(true);
         //RTSCameraController.instance.stopWork = true;
         Time.timeScale = 0f;
+        isPaused = true;
     }
+    
     public void ContinueButton()
     {
         Time.timeScale = 1f;
         //RTSCameraController.instance.stopWork = false;
         pausePanel.SetActive(false);
+        isPaused = false;
     }
+    
     public void MenuBackButton()
     {
         
