fcd1922b67ac74a1d99f118f20273ba8c41762bc
XamidovDoniyor22 <khamidovdoniyorbek022@gmail.com>
2025-03-15 17:33:42 +0500

    Error Fix

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -10,13 +10,13 @@ public class PauseScript : MonoBehaviour
     {
         Debug.Log("Pause");
         pausePanel.SetActive(true);
-        RTSCameraController.instance.stopWork = true;
+        //RTSCameraController.instance.stopWork = true;
         Time.timeScale = 0f;
     }
     public void ContinueButton()
     {
         Time.timeScale = 1f;
-        RTSCameraController.instance.stopWork = false;
+        //RTSCameraController.instance.stopWork = false;
         pausePanel.SetActive(false);
     }
     public void MenuBackButton()
